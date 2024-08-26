f590d9edcaaa7605074bce580f7422ff32ab3f5b
Kilfaera <106203422+Kilfaera@users.noreply.github.com>
2024-08-26 17:35:56 -0500

    Add files via upload

---
+++ Capa_Control/CL_Consumo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumos_Sermopetrol.Capa_Control
+{
+    internal class CL_Consumo
+    {
+    }
+}
