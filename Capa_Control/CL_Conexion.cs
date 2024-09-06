265e71138a96b89b5c7cd2c12b3a221f813dc3df
MrVsps <xxxcamilo31@gmail.com>
2024-09-06 09:09:54 -0500

    cambios en conexion

--- Capa_Control/CL_Conexion.cs
+++ Capa_Control/CL_Conexion.cs
@@ -8,5 +8,6 @@ namespace Consumos_Sermopetrol.Capa_Control
 {
     internal class CL_Conexion
     {
+        public static string cadena = "Server=127.0.0.1;Port=3306;Database=ConsumoEmpleado;Uid=root;Pwd=Sermopetrol;";
     }
 }
